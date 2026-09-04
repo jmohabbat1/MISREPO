9b39b5e5b68ad6ed3ec698d31dbe9de15fe81752
Jacob Mohabbat <jacobmohabbat@Jacobs-MacBook-Air.local>
2026-09-04 17:02:34 -0500

    Did the value-adds

--- Desktop/MIS221/lab2/app.py
+++ Desktop/MIS221/lab2/app.py
@@ -8,3 +8,15 @@ print("Total amount:", f"{subtotal + tax_amount + tip_amount:.2f}")
 print("Amount per person:", f"{(subtotal + tax_amount + tip_amount) / num_people:.2f}")
 total_bill = subtotal + tax_amount + tip_amount
 per_person = total_bill / num_people
+
+#Value add
+
+drink_subtotal = float(input("Enter the drink subtotal: "))
+drink_tax_amount = drink_subtotal * 0.085
+total_drink_amount = drink_subtotal + drink_tax_amount
+
+print("Drink subtotal", f"{drink_subtotal:.2f}") 
+print("Drink tax amount", f"{drink_tax_amount:.2f}")
+print("Amount per person for second round of drinks:", f"{(total_drink_amount + drink_tax_amount) / num_people:.2f}")
+print("Total amount per person including drinks:", f"{(total_bill + total_drink_amount)/ num_people:.2f}")
+
